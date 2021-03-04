c0b94f20c527cfbf55a27d09a94e61f995bff7a2
vuthienson8 <thiensonvu08@gmail.com>
2021-03-04 22:44:03 +0700

    login View

--- my_app/application/lib/page/login/login_view.dart
+++ my_app/application/lib/page/login/login_view.dart
@@ -10,59 +10,77 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
+      body: LayoutBuilder(
+        builder: (context, constraints) => Stack(
           children: [
-            Container(), //tao background
+            Positioned.fill(
+              child: Container(color: Colors.blue),
+            ),
+            Positioned.fill(
+              top: MediaQuery.of(context).size.height / 3,
+              child: Container(color: Colors.white),
+            ),
             ListView(
               children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      width: 500,
-                      child: Card(
-                        elevation: 5,
-                        child: Padding(
-                          padding: EdgeInsets.all(32),
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                key: Key('email'),
-                                decoration: InputDecoration(
-                                    labelText: 'Email',
-                                    icon: Icon(Icons.email_outlined)),
-                              ),
-                              TextFormField(
-                                key: Key('password'),
-                                decoration: InputDecoration(
-                                  labelText: 'Password',
-                                  icon: Icon(Icons.security),
-                                ),
-                                obscureText: true,
-                              ),
-                              SizedBox(height: 20),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 500,
+                          child: Card(
+                            elevation: 5,
+                            child: Padding(
+                              padding: EdgeInsets.all(32),
+                              child: Column(
                                 children: [
-                                  RaisedButton(
-                                    onPressed: () {},
-                                    child: Text('Login'),
+                                  TextFormField(
+                                    key: Key('email'),
+                                    decoration: InputDecoration(
+                                        labelText: 'Email',
+                                        icon: Icon(Icons.email_outlined)),
                                   ),
-                                  TextButton(
-                                      onPressed: () {},
-                                      child: Text('Forgot Password?'))
+                                  TextFormField(
+                                    key: Key('password'),
+                                    decoration: InputDecoration(
+                                      labelText: 'Password',
+                                      icon: Icon(Icons.security),
+                                    ),
+                                    obscureText: true,
+                                  ),
+                                  SizedBox(height: 20),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      RaisedButton(
+                                        onPressed: () {},
+                                        child: Text('Login'),
+                                        color: Colors.blue,
+                                      ),
+                                      TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Forgot Password?',
+                                        ),
+                                      )
+                                    ],
+                                  ),
                                 ],
                               ),
-                            ],
+                            ),
                           ),
-                        ),
-                      ),
-                    )
-                  ],
+                        )
+                      ],
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
